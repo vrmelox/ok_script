ed0098f24def2e0a5ae193752cb79d6334da66b9
Philippe Akandé <akandeabiodoun@gmail.com>
2024-08-12 08:18:24 +0100

    the connexion page is now completed

--- connexion.php
+++ connexion.php
@@ -29,37 +29,5 @@
                 </a>
             </div>
         </form>
-        <form action="" class="sign-up-form">
-            <h2 class="title">Sign up</h2>
-            <div class="input-field">
-                <i class="fas fa-user"></i>
-                <input type="text" placeholder="Username">
-            </div>
-            <div class="input-field">
-                <i class="fas fa-envelope"></i>
-                <input type="text" placeholder="Email">
-            </div>
-            <div class="input-field">
-                <i class="fas fa-lock"></i>
-                <input type="password" placeholder="Password">
-            </div>
-            <input type="submit" value="sign up" class="btn">
-            <p class="social-text">Or sign in with social plateform</p>
-            <div class="social-media">
-                <a href="" class="social-icon">
-                    <i class="fab fa-facebook"></i>
-                </a>
-                <a href="" class="social-icon">
-                <i class="fab fa-twitter"></i>
-                </a>
-                <a href="" class="social-icon">
-                <i class="fab fa-google"></i>
-                </a>
-                <a href="" class="social-icon">
-                <i class="fab fa-linkedin-in"></i>
-                </a>
-            </div>
-        </form>
     </div>
-    <div class="panels-container"></div>
 </div>
